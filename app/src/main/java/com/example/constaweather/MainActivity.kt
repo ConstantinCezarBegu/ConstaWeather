5731a3a674d300e3db6f0f3fb57b3ee09946d1f1
ConstantinCezB <Constantin.Cez.b@gmail.com>
2019-05-30 22:51:23 -0400

    A=Created the layout of the files and attached the navigation.

--- app/src/main/java/com/example/constaweather/MainActivity.kt
+++ app/src/main/java/com/example/constaweather/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.constaweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.constaweather.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // This is used to control the navigation with the xml file
+    // The id of the item list and the fragments must be the same
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        // Attaches the bottom bar with the navigation controller so that they can interact
+        bottom_nav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        //This function is sets the back button in the toolbar
+        return NavigationUI.navigateUp(navController, null)
     }
 }
---
+++ app/src/main/java/com/example/constaweather/ui/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.constaweather.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
